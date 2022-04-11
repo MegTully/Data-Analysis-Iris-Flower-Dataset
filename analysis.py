2bc1345286b81fcf7c8cb073c03349283176737e
MegTully <g00411235@gmit.ie>
2022-04-11 15:42:53 +0100

    Dataset edited

---
+++ analysis.py
@@ -0,0 +1,10 @@
+#Need to use package pandas
+import pandas as pd
+import matplotlib as plt
+
+#read in the iris dataset and assign it a avariable name data
+data = pd.read_csv("C:/Users/Owner/Desktop/pands-project/pands-project/pands-project/iris.txt")
+
+print(data.head())
+
+
