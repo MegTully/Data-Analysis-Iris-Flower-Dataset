773912ea110b20a652b3be316095d77f47d976f7
MegTully <g00411235@gmit.ie>
2022-04-11 16:16:31 +0100

    SepalLength stats

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This Project is based off a dataset created by R.A Fisher which contains information about three types of iris plants. There are 4 attributes used to differentiate each plant class, they are petal length, petal width, sepal length and sepal width. The three iris flower types are iris setosa, iris versicolor and iris virginica[1].
 
+# Iris Dataset
+The dataset was downloaded from the UCI website [1] and column headings were added to the .txt file in notepad ++. The dataset was then read into the analysis.py file to be analysed.
+
 
 
 
--- analysis.py
+++ analysis.py
@@ -1,10 +1,32 @@
 #Need to use package pandas
 import pandas as pd
-import matplotlib as plt
+import numpy as np
 
-#read in the iris dataset and assign it a avariable name data
+#read in the iris dataset and assign it a avariable name data [1]
 data = pd.read_csv("C:/Users/Owner/Desktop/pands-project/pands-project/pands-project/iris.txt")
 
-print(data.head())
+#Summary of Variable SepalLenth[1]
+mean_SL = data["SepalLength"].mean()
+median_SL = data["SepalLength"].median()
+min_SL = data["SepalLength"].min()
+max_SL = data["SepalLength"].max()
+
+#print out the results and round the mean to one decimal place
+print("Mean:", round(mean_SL,1), "\nMedian:",median_SL, "\nMin:", min_SL, "\nMax:", max_SL)
+
+#Using numpy to get standard deviation and variance rounded to 2 decimal places[2]
+std_SL = np.std(data["SepalLength"])
+var_SL = np.var(data["SepalLength"])
+
+print("Standard Deviation:",round(std_SL,2), "\nVariance:",round(var_SL, 2) )
 
 
+
+
+ 
+#print(data.shape)
+
+
+#References:
+#[1]https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
+#[2]https://www.w3schools.com/python/python_ml_standard_deviation.asp
